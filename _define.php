d06095c3847b77f50409888d841a19282caf91bd
Pascal Corpet <pascal@corpet.net>
2015-12-10 11:28:52 +0100

    Start a new dotclear plugin.

---
+++ _define.php
@@ -0,0 +1,15 @@
+<?php
+if (!defined('DC_RC_PATH')) {return;}
+
+$this->registerModule(
+	/* Name */ 'dotsphere',
+	/* Description */ 'Adds a button to easily insert a PhotoSphere inside a post',
+	/* Author */ 'Pascal Corpet',
+	/* Version */ '0.1',
+	array(
+		'type' => 'plugin',
+		'permissions' => 'usage,contentadmin'
+	)
+);
+
+?>
